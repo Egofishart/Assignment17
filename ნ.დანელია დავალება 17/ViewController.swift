c8e6e900023f456317ed0f2d5cc07d71aee7cf21
Egofishart <nikodanelia16@gmail.com>
2026-07-16 01:33:11 +0400

    Initial commit

---
+++ ნ.დანელია დავალება 17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ნ.დანელია დავალება 17
+//
+//  Created by NikoDanelia on 16/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
